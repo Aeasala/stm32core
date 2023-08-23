7413e4cc6b09d983f20aac9be7f97580ff6e8cda
evanm <esmacdon@gmail.com>
2023-08-23 13:04:19 -0400

    Final touch-ups before PR.

--- targets.mk
+++
@@ -1,8 +0,0 @@
-CFLAGS_TARG =
-
-BUILD_CORTEX_M0 := true
-
-#add lib(s) according to flags
-ifeq ($(BUILD_CORTEX_M0), true)
-CFLAGS_TARG+=-mlittle-endian -mcpu=cortex-m0  -march=armv6-m -mthumb
-endif
